6960ce76b40001999613ec4a4c6ece31990c7ae6
Brice Chapuis <bricechapuis>
2021-04-12 09:03:23 +0200

    Safari bug fix

--- _site/assets/javascripts/homeseven.js
+++ _site/assets/javascripts/homeseven.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-seven');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight*.95) {
+      if (window.scrollY >= window.innerHeight*.90) {
         navbar.classList.add('scrolled');
       } else {
         navbar.classList.remove('scrolled');
